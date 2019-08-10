223281efb76b6156650841c481ada243154b6799
talharahman <talharahman@pursuit.org>
2019-08-10 14:11:33 -0400

    add subview to home and fav

--- app/src/main/java/com/example/pursuitdemoapp/mobile/ui/FavoritesView.java
+++ app/src/main/java/com/example/pursuitdemoapp/mobile/ui/FavoritesView.java
@@ -3,13 +3,37 @@ package com.example.pursuitdemoapp.mobile.ui;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.widget.FrameLayout;
+import android.widget.TextView;
+import com.example.pursuitdemoapp.R;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class FavoritesView extends FrameLayout {
 
+    private RecyclerView favoritesRecyclerView;
+    private TextView emptyView;
+
     public FavoritesView(@NonNull Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+    }
+
+    @Override protected void onFinishInflate() {
+        super.onFinishInflate();
+
+        favoritesRecyclerView = findViewById(R.id.favorites);
+        emptyView = findViewById(R.id.empty);
+
+        favoritesRecyclerView.setLayoutManager(
+                new LinearLayoutManager(getContext(), RecyclerView.HORIZONTAL, false));
+    }
+
+    @Override protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+
+        favoritesRecyclerView.setVisibility(GONE);
+        emptyView.setVisibility(VISIBLE);
     }
 }
--- app/src/main/java/com/example/pursuitdemoapp/mobile/ui/HomeView.java
+++ app/src/main/java/com/example/pursuitdemoapp/mobile/ui/HomeView.java
@@ -3,13 +3,31 @@ package com.example.pursuitdemoapp.mobile.ui;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.widget.LinearLayout;
+import com.example.pursuitdemoapp.R;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class HomeView extends LinearLayout {
 
+    private RecyclerView nowPlayingRecyclerView;
+    private RecyclerView mostPopularRecyclerView;
+
     public HomeView(@NonNull Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+    }
+
+    @Override protected void onFinishInflate() {
+        super.onFinishInflate();
+
+        nowPlayingRecyclerView = findViewById(R.id.now_playing);
+        mostPopularRecyclerView = findViewById(R.id.most_popular);
+
+        nowPlayingRecyclerView.setLayoutManager(
+                new LinearLayoutManager(getContext(), HORIZONTAL, false));
+        mostPopularRecyclerView.setLayoutManager(
+                new LinearLayoutManager(getContext(), HORIZONTAL, false));
     }
 }
